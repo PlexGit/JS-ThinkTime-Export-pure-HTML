41473352de6d1336472241c5827017eaee575290
PlexGit <103202269+PlexGit@users.noreply.github.com>
2026-03-13 18:49:08 +0200

    Update version and URLs in user script

--- ThinkTime - Add Print button for Knowlege Base articles.user.js
+++ ThinkTime - Add Print button for Knowlege Base articles.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          ThinkTime - Add Print View button for Knowlege Base articles
 // @namespace     http://tampermonkey.net/
-// @version       1.02
+// @version       1.03
 // @description   Add Print View button for Knowlege Base articles on ThinkTime platform
 // @author        Oleksandr Pylypchak
 // @match         https://*.thinktime.com/ui/knowledge-bases/*/articles/*
@@ -10,8 +10,8 @@
 // @match         https://*.thinktime.com/ui/news/articles/*
 // @icon          https://www.google.com/s2/favicons?sz=64&domain=thinktime.com
 // @grant         none
-// @updateURL     https://gist.github.com/PlexGit/db248956219578ca1620c9493a92c8a0/raw/ThinkTime%2520-%2520Add%2520Print%2520button%2520for%2520Knowlege%2520Base%2520articles.user.js
-// @downloadURL   https://gist.github.com/PlexGit/db248956219578ca1620c9493a92c8a0/raw/ThinkTime%2520-%2520Add%2520Print%2520button%2520for%2520Knowlege%2520Base%2520articles.user.js
+// @updateURL     https://github.com/PlexGit/JS---ThinkTime---Add-Print-button-for-Knowlege-Base-articles/raw/refs/heads/main/ThinkTime%20-%20Add%20Print%20button%20for%20Knowlege%20Base%20articles.user.js
+// @downloadURL   https://github.com/PlexGit/JS---ThinkTime---Add-Print-button-for-Knowlege-Base-articles/raw/refs/heads/main/ThinkTime%20-%20Add%20Print%20button%20for%20Knowlege%20Base%20articles.user.js
 // ==/UserScript==
 
 (function() {
